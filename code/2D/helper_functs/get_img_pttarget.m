ecd53456fcfeb08d3a787943b46af59f5835ffc1
wjkaufman <will@wjkaufman.com>
2019-05-18 20:11:27 -0400

    add point target function generator

---
+++ code/2D/helper_functs/get_img_pttarget.m
@@ -0,0 +1,35 @@
+function [ f ] = get_img_pttarget(N, numTargets, changePct)
+% N: dimension of image
+% numTargets: number of targets to include
+% changePct: rough percentage of targets to delete/move (0 is no changes)
+
+f = zeros([N, N]);
+
+gridLength = floor(N/ceil(sqrt(numTargets)));
+
+x=-round(gridLength/2); y=-x;
+for i=1:numTargets
+    % increment to next point
+    x = x + gridLength;
+    if x > N
+        x = round(gridLength/2);
+        y = y + gridLength;
+        if y > N
+            break
+        end
+    end
+    if changePct > rand()
+        % make a change to whether the target is on the grid
+        dx = randi([-5,5]); dy = randi([-5,5]);
+        if 0.2 > rand()
+            continue % don't put in target
+        else
+            f(x+dx, y+dy) = 1;
+        end
+    else
+        f(x,y) = 1;
+    end
+end
+
+end
+
